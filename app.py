00142053e1269f88bc33c3f8afecceff1a920f4a
sezaz <sezazqureshi@gmail.com>
2021-08-24 14:44:13 +0530

    all done

--- app.py
+++ app.py
@@ -1,7 +1,5 @@
-<<<<<<< HEAD
+
 print('hello') 
 print("this is a python file normally")
-=======
 print('hello')
-print('this is the clash ")
->>>>>>> c4b08701736715e0f9f20d5311098b8e1a4a9c0e
+print('this is the clash ')
